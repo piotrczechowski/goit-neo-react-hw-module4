f902d8247043dbd1468bf950603e9484852da0d1
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2025-03-03 21:14:21 +0100

    add clip loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Loader = () => {
   return (
-    <div>
-      <p>Loading... </p>
+    <div style={{ textAlign: "center", margin: "20px 0" }}>
+      <ClipLoader color="#00BFFF" size={50} />
     </div>
   );
 };
